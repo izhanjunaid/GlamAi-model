daf754c4025c9d1eaecf7d4c63620d9ff88c04e0
Ekta Gavas <ekta.gavas96@gmail.com>
2024-07-15 13:54:35 -0400

    Updated for multiple datasets

--- scripts/demo.py
+++ scripts/demo.py
@@ -5,6 +5,7 @@ import numpy as np
 import cv2
 import torch
 from PIL import Image
+import glob
 sys.path.append('.')
 
 from training.config import get_config
@@ -18,34 +19,37 @@ def main(config, args):
 
     inference = Inference(config, args, args.load_path)
 
-    n_imgname = sorted(os.listdir(args.source_dir))
-    m_imgname = sorted(os.listdir(args.reference_dir))
+    # n_imgname = sorted(os.listdir(args.source_dir))
+    # m_imgname = sorted(os.listdir(args.reference_dir))
     
-    for i, imga_name in enumerate(n_imgname):
-      for j, imgb_name in enumerate(m_imgname):
-        imgA = Image.open(os.path.join(args.source_dir, imga_name)).convert('RGB')
-        imgB = Image.open(os.path.join(args.reference_dir, imgb_name)).convert('RGB')
-        # imgA = cv2.imread(imgA)
+    # for i, imga_name in enumerate(n_imgname):
+    for i, imga_name in enumerate(glob.glob(args.source_dir+'/*/female/*')):
+        if '.jpg' in imga_name or '.png' in imga_name or '.JPG' in imga_name:
+          print(imga_name)
+          for j, imgb_name in enumerate(glob.glob(args.reference_dir+'/*.png')):
+            imgA = Image.open(os.path.join(args.source_dir, imga_name)).convert('RGB')
+            imgB = Image.open(os.path.join(args.reference_dir, imgb_name)).convert('RGB')
+            # imgA = cv2.imread(imgA)
 
-        result = inference.transfer(imgA, imgB, postprocess=True) 
-        if result is None:
-            continue
-        imgA = np.array(imgA); imgB = np.array(imgB)
-        h, w, _ = imgA.shape
-        # result = result.resize((h, w)); 
-        result = np.array(result)
-        # vis_image = np.hstack((imgA, imgB, result))
-        save_path = os.path.join(args.save_folder, f"result_{i}{j}.png")
-        Image.fromarray(result.astype(np.uint8)).save(save_path)
+            result = inference.transfer(imgA, imgB, postprocess=True) 
+            if result is None:
+                continue
+            imgA = np.array(imgA); imgB = np.array(imgB)
+            h, w, _ = imgA.shape
+            # result = result.resize((h, w)); 
+            result = np.array(result)
+            # vis_image = np.hstack((imgA, imgB, result))
+            save_path = imga_name.replace('selected', 'output').split('.')[0]+'-'+os.path.basename(imgb_name).split('.')[0]+'.png'
+            os.makedirs(os.path.dirname(save_path), exist_ok=True)
+            Image.fromarray(result.astype(np.uint8)).save(save_path)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("argument for training")
-    parser.add_argument("--name", type=str, default='demo')
-    parser.add_argument("--save_path", type=str, default='result', help="path to save model")
+    parser.add_argument("--name", type=str, default='cacd')
+    parser.add_argument("--save_path", type=str, default='/content/output', help="path to save model")
     parser.add_argument("--load_path", type=str, help="folder to load model", 
                         default='ckpts/sow_pyramid_a5_e3d2_remapped.pth')
-
     parser.add_argument("--source-dir", type=str, default="assets/images/non-makeup")
     parser.add_argument("--reference-dir", type=str, default="assets/images/makeup")
     parser.add_argument("--gpu", default='0', type=str, help="GPU id to use.")
@@ -56,9 +60,8 @@ if __name__ == "__main__":
     else: args.gpu = 'cpu'
     args.device = torch.device(args.gpu)
 
-    args.save_folder = os.path.join(args.save_path, args.name)
-    if not os.path.exists(args.save_folder):
-        os.makedirs(args.save_folder)
+    args.save_folder = os.path.join(args.save_path+'-'+args.name)
+    os.makedirs(args.save_folder, exist_ok=True)
     
     config = get_config()
     main(config, args)
